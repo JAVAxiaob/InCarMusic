afef6adc0685c747cd9d2c06d4283d7d80d1cdaa
yjc <1980024346@qq.com>
2024-09-20 16:46:40 +0800

    Initial commit

---
+++ automotive/src/main/java/com/example/incarmusicsystem/Video/Service/MyPlayerService.java
@@ -0,0 +1,9 @@
+package com.example.incarmusicsystem.Video.Service;
+
+import snow.player.PlayerService;
+import snow.player.annotation.PersistenceId;
+
+@PersistenceId("MyPlayerService")
+public class MyPlayerService extends PlayerService {
+
+}
